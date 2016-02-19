3d2795c0560e0da92c0f9283d5a902b5a690e52f
Stuart Fotheringham <stuart.angus.fotheringham@gmail.com>
2016-02-20 01:37:08 +1030

    json traversing property tree arrays

--- json/test_json.cpp
+++ json/test_json.cpp
@@ -12,34 +12,105 @@
 #define STR(x) #x << '=' << x
 #define HERE() __FUNCTION__ << '(' << __LINE__ << ')'
 
-int main()
+int
+main ()
 {
-  boost::property_tree::ptree pt;
-  std::string json("{ \"time\": \"values\",\"date\":\"1234\" }");
-  std::istringstream input(json);
+  {
+    boost::property_tree::ptree pt;
+    std::string json ("{ \"time\": \"values\",\"date\":\"1234\" }");
+    std::istringstream input (json);
 
-  try {
-    boost::property_tree::read_json(input, pt);
-  }
-  catch (boost::property_tree::json_parser_error& e) {
-    std::cerr << HERE() << ':' << "error\n";
-    return -1;
+    try
+    {
+      boost::property_tree::read_json (input, pt);
+    }
+    catch (boost::property_tree::json_parser_error& e)
+    {
+      std::cerr << HERE() << ':' << "error\n";
+      return -1;
+    }
+
+    try
+    {
+      std::string str = pt.get<std::string> ("time");
+      std::cout << STR(str) << '\n';
+
+      str = pt.get<std::string> ("date");
+      std::cout << STR(str) << '\n';
+    }
+    catch (boost::property_tree::ptree_bad_path& e)
+    {
+      std::cerr << HERE() << ':' << "error\n";
+      return -1;
+    }
+    catch (boost::property_tree::ptree_bad_data& e)
+    {
+      std::cerr << HERE() << ':' << "error\n";
+      return -1;
+    }
   }
 
-  try {
-    std::string str = pt.get<std::string>("time");
-    std::cout << STR(str) << '\n';
+  {
+    boost::property_tree::ptree pt;
+    std::string json ("{ \"root\": { \"values\": [1, 2, 3, 4, 5 ] } }");
+    //std::string json ("{ \"root\": { \"values\": \"1\" } }");
+    std::istringstream input (json);
 
-    str = pt.get<std::string>("date");
-    std::cout << STR(str) << '\n';
+    try
+    {
+      boost::property_tree::read_json (input, pt);
+    }
+    catch (boost::property_tree::json_parser_error& e)
+    {
+      std::cerr << HERE() << ':' << "error\n";
+      return -1;
+    }
+
+    try
+    {
+      for (auto& child : pt.get_child("root.values"))
+      {
+        std::cout << child.first << "\n";
+        std::cout << child.second.data() << "\n";
+      }
+    }
+    catch (boost::property_tree::json_parser_error& e)
+    {
+      std::cerr << HERE() << ':' << "error " << e.message() << "\n";
+      return -1;
+    }
   }
-  catch (boost::property_tree::ptree_bad_path& e) {
-    std::cerr << HERE() << ':' << "error\n";
-    return -1;
+
+  {
+    boost::property_tree::ptree pt;
+    std::string json ("{ \"root\": { \"name\": \"John\", \"age\": 22 } }");
+    //std::string json ("{ \"root\": { \"values\": \"1\" } }");
+    std::istringstream input (json);
+
+    try
+    {
+      boost::property_tree::read_json (input, pt);
+    }
+    catch (boost::property_tree::json_parser_error& e)
+    {
+      std::cerr << HERE() << ':' << "error\n";
+      return -1;
+    }
+
+    try
+    {
+      for (auto& child : pt.get_child("root"))
+      {
+        std::cout << child.first << "\n";
+        std::cout << child.second.data() << "\n";
+      }
+    }
+    catch (boost::property_tree::json_parser_error& e)
+    {
+      std::cerr << HERE() << ':' << "error " << e.message() << "\n";
+      return -1;
+    }
   }
-  catch (boost::property_tree::ptree_bad_data& e) {
-    std::cerr << HERE() << ':' << "error\n";
-    return -1;
-  }
+
   return 0;
 }
