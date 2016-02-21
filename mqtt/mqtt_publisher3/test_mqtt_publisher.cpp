21d84ab55b4d360c72a46c0fca99c1a080feb54a
Stuart Fotheringham <stuart.angus.fotheringham@gmail.com>
2016-02-22 00:11:31 +1030

    Command line arguments for MQTT Subscriber

--- mqtt/mqtt_publisher3/test_mqtt_publisher.cpp
+++ mqtt/mqtt_publisher3/test_mqtt_publisher.cpp
@@ -1,14 +1,81 @@
 #include <thread>
 #include <sstream>
+#include <popt.h>
 #include <unistd.h> // sleep
 #include "mqtt_publisher.h"
 
+enum {
+  OPTION_HELP = 1,
+  OPTION_BROKER,
+  OPTION_PORT,
+  OPTION_TOPIC,
+  OPTION_QOS,
+};
+
+static void
+parse_command_line(int argc, char **argv, std::string& broker, int& port,
+                   std::string& topic, int& qos)
+{
+  char *brokerPtr = nullptr;
+  char *topicPtr = nullptr;
+
+  struct poptOption options[] = {
+      {"help", 'h', POPT_ARG_NONE, 0, OPTION_HELP, "print this message", 0 },
+      {"broker", 'b', POPT_ARG_STRING, &brokerPtr, OPTION_BROKER, "broker name", "name" },
+      {"port", 'p', POPT_ARG_INT, &port, OPTION_PORT, "set the MQTT port", "number" },
+      {"topic", 't', POPT_ARG_STRING, &topicPtr, OPTION_TOPIC, "topic", "topic" },
+      {"qos", 'q', POPT_ARG_INT, &qos, OPTION_QOS, "quality of service (0,1,2). Should precede topic", "number" },
+      POPT_AUTOHELP
+      { 0, 0, 0, 0, 0, 0, 0 }
+  };
+
+  poptContext context;
+  context = poptGetContext(0, argc, (const char **)argv, options, 0);
+
+  while (1)
+  {
+    int rc = poptGetNextOpt(context);
+
+    if (rc > 0) {
+      switch (rc) {
+      case OPTION_HELP:
+        poptPrintHelp(context, stdout, 0);
+        exit(0);
+      case OPTION_BROKER:
+        broker = brokerPtr;
+        break;
+      case OPTION_PORT:
+        break;
+      case OPTION_TOPIC:
+        topic = topicPtr;
+        break;
+      case OPTION_QOS:
+        break;
+      default:
+        fprintf(stderr, "Unknown switch\n");
+        exit(-1);
+      }
+    } else if (rc < -1) {
+      fprintf(stderr, "Unknown parameter\n");
+      exit(-1);
+    } else {
+      break;
+    }
+  }
+}
+
 int main(int argc, char **argv)
 {
+  std::string broker("localhost");
+  int port = 1883;
+  std::string topic("hello/world");
+  int qos = 0;
+  parse_command_line(argc, argv, broker, port, topic, qos);
+
   conditioned_list<TopicPayloadQos> input_list;
 
-  input_list.push_back(TopicPayloadQos("temperature/celsius","hello",0));
-  input_list.push_back(TopicPayloadQos("temperature/celsius","world",0));
+  input_list.push_back(TopicPayloadQos(topic,"hello",0));
+  input_list.push_back(TopicPayloadQos(topic,"world",0));
 
   std::thread mqtt_publisher_thread(run_mqtt_publisher, "localhost", 1883, &input_list);
 
@@ -16,13 +83,13 @@ int main(int argc, char **argv)
   for (;;) {
     std::ostringstream oss;
     oss << "count=" << count++;
-    input_list.push_back(TopicPayloadQos("temperature/celsius",oss.str(),0));
+    input_list.push_back(TopicPayloadQos(topic,oss.str(),0));
     if ((count % 10) == 0)
       sleep(2); // cause timeout
     else
       usleep(100000);
     if (count == 30) {
-      input_list.push_back(TopicPayloadQos("temperature/celsius",oss.str(),-1)); // quit
+      input_list.push_back(TopicPayloadQos(topic,oss.str(),-1)); // quit
       break;
     }
   }
