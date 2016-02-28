6612a070801d35517fede183e0aaa03c0d1733d7
Stuart Fotheringham <stuart.angus.fotheringham@gmail.com>
2016-02-28 22:23:20 +1030

    Commit some lines

---
+++ wav_files/middle_c/middleC.cpp
@@ -0,0 +1,30 @@
+/*
+ * middleC.cpp
+ *
+ *  Created on: 28 Feb 2016
+ *      Author: stuartf
+ */
+
+int main()
+{
+  int N = hz * seconds;  // total number of samples
+  for (int n = 0; n < N; n++)
+  {
+    double amplitude = (double)n / N * max_amplitude;
+    //double amplitude = (double)max_amplitude;
+    double value     = sin( (two_pi * n * frequency) / hz );
+    write_word( f, (int)(                 amplitude  * value), 2 );
+    write_word( f, (int)((max_amplitude - amplitude) * value), 2 );
+  }
+
+  // (We'll need the final file size to fix the chunk sizes above)
+  size_t file_length = f.tellp();
+
+  // Fix the data chunk header to contain the data size
+  f.seekp( data_chunk_pos + 4 );
+  write_word( f, file_length - data_chunk_pos + 8 );
+
+  // Fix the file header to contain the proper RIFF chunk size, which is (file size - 8) bytes
+  f.seekp( 0 + 4 );
+  write_word( f, file_length - 8, 4 );
+}
