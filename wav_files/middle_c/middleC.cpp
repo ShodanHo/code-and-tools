6a68bb8cc902d2e4bd40db9696b39208bde952fd
Stuart Fotheringham <stuart.angus.fotheringham@gmail.com>
2016-02-28 22:25:44 +1030

    Commit some more lines

--- wav_files/middle_c/middleC.cpp
+++ wav_files/middle_c/middleC.cpp
@@ -19,6 +19,17 @@
 using namespace std;
 
 namespace little_endian_io
+{
+  template <typename Word>
+  std::ostream& write_word( std::ostream& outs, Word value, unsigned size = sizeof( Word ) )
+  {
+    for (; size; --size, value >>= 8)
+      outs.put( static_cast <char> (value & 0xFF) );
+    return outs;
+  }
+}
+using namespace little_endian_io;
+
 int main()
 {
   // char                Subchunk2ID[4]; // "data"  string
