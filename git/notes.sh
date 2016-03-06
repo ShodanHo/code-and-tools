e7d19b5369224d18b600241cbee6d53c19656480
Stuart Fotheringham <stuart.angus.fotheringham@gmail.com>
2016-03-06 23:04:01 +1030

    More git

--- git/notes.sh
+++ git/notes.sh
@@ -8,5 +8,15 @@ alias git-ls='git log --decorate --oneline'
 alias git-ll='git log --decorate --numstat'
 
 # roll commit into previous commit
-git commit --amend -C HEAD
+git commit --amend -C HEAD # reuse message
+git commit --amend -c HEAD # change message
 
+
+# move HEAD back and forward
+git reset HEAD~
+git reset HEAD~1
+
+git reflog
+git reset HEAD@{2}
+git reflog show master
+git reset ORIG_HEAD
