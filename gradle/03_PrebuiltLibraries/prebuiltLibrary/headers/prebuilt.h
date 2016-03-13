faa5d4ccb8305e028f8870afa2678356ed608b7f
Stuart Fotheringham <stuart.angus.fotheringham@gmail.com>
2016-03-13 20:00:48 +1030

    Lesson from C++ video on pre-built libraries

---
+++ gradle/03_PrebuiltLibraries/prebuiltLibrary/headers/prebuilt.h
@@ -0,0 +1,6 @@
+#ifndef __PREBUILT_H__
+#define __PREBUILT_H__
+
+int add(int a, int b);
+
+#endif // __PREBUILT_H__
---
+++ gradle/03_PrebuiltLibraries/prebuiltLibrary/src/prebuilt.cpp
@@ -0,0 +1,6 @@
+#include "prebuilt.h"
+
+int add(int a, int b)
+{
+  return a + b;
+}
