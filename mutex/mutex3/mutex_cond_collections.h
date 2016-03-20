987adf344142446ddebcab707efcc142063c0dc0
Stuart Fotheringham <stuart.angus.fotheringham@gmail.com>
2016-03-21 02:01:18 +1030

    referenced_cond_mutex

--- mutex/mutex3/mutex_cond_collections.h
+++ mutex/mutex3/mutex_cond_collections.h
@@ -54,4 +54,66 @@ public:
   }
 };
 
+/**
+ * Similar to conditioned_list, but with references to the mutex and condition.
+ * This enables sharing of these variables amongst multiple collection class
+ * instances, so that one listener can block on all of them.
+ */
+template <typename X>
+class referenced_conditioned_list
+{
+  std::mutex &mtx;
+  std::condition_variable &cv; //!< set on push_back()
+  std::list<X> xList; //!< the std::list
+
+public:
+  referenced_conditioned_list(std::mutex &_mtx, std::condition_variable &_cv) : mtx(_mtx), cv(_cv), xList() {}
+
+  //! equivalent of std::list<>.push_back()
+  void push_back(const X& x)
+  {
+    std::unique_lock<std::mutex> lck(mtx);
+    xList.push_back(x);
+    cv.notify_one();
+  }
+
+  //! equivalent of std::list<>.size()
+  typename std::list<X*>::size_type size() const noexcept
+  {
+    std::unique_lock<std::mutex> lck(mtx);
+    return xList.size();
+  }
+
+  //! return front(). not a reference!!
+  X front_value()
+  {
+    std::unique_lock<std::mutex> lck(mtx);
+    X ret = xList.front();
+    return ret;
+  }
+
+  //! equivalent of std::list<>.pop_front()
+  void pop_front()
+  {
+    std::unique_lock<std::mutex> lck(mtx);
+    xList.pop_front();
+  }
+
+  //! block on condition being set
+  void cond_wait()
+  {
+    std::unique_lock<std::mutex> lck(mtx);
+    cv.wait(lck);
+  }
+
+  //! block for a period on condition being set
+  template <class Rep, class Period = std::ratio<1> >
+  std::cv_status
+  cond_wait_for(const std::chrono::duration<Rep,Period>& rel_time)
+  {
+    std::unique_lock<std::mutex> lck(mtx);
+    return cv.wait_for(lck,rel_time);
+  }
+};
+
 #endif // __MUTEX_COND_COLLECTIONS_H__
